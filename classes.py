6e1a006f2a908403234b69455707900933e2a012
Felipe Oliveira <felipe.c@outlook.com>
2023-02-08 23:46:36 -0300

    Atualização de código

--- classes.py
+++ classes.py
@@ -1,5 +1,7 @@
-from openpyxl.styles import Font
+from openpyxl.chart import LineChart, Reference
+from openpyxl.drawing.image import Image
 from openpyxl.styles import PatternFill, Font, Alignment
+from openpyxl.workbook import Workbook
 
 
 class LeitorAcoes:
@@ -12,6 +14,10 @@ class LeitorAcoes:
             linhas = arquivo_cotacao.readlines()
             self.dados = [linha.replace("\n", "").split(";") for linha in linhas]
 
+class PropriedadesSerieGradicos:
+    def __init__(self, grossura: int, cor_preenchimento: str):
+        self.grossura = grossura
+        self.cor_preenchimento = cor_preenchimento
 
 class GerenciadorPlanilha:
     def __init__(self):
@@ -32,7 +38,43 @@ class GerenciadorPlanilha:
         self.planilha_ativa[celula] = dado
 
     def mescla_celula(self, celula_inicio: str, celula_fim: str):
-        self.planilha_ativa.merge.cells(f'{celula_inicio}:{celula_fim}')
+        self.planilha_ativa.merge_cells(f'{celula_inicio}:{celula_fim}')
 
-    def estiliza_fonte(self, celula: str, fonte: Font):
-        self.planilha_ativa[celula].font = fonte
+    # def estiliza_fonte(self, celula: str, fonte: Font):
+    #     self.planilha_ativa[celula].font = fonte
+    #
+    # def estiliza_preenchimento(self, celula: str, preenchimento: PatternFill):
+    #     self.planilha_ativa[celula].fill = preenchimento
+    #
+    # def estiliza_alinhamento(self, celula: str, alinhamento: Alignment):
+    #     self.planilha_ativa[celula].alignment = alinhamento
+
+    def aplica_estilos(self, celula: str, estilos: list):
+        for estilo in estilos:
+            setattr(self.planilha_ativa[celula], estilo[0], estilo[1])
+
+    def adiciona_grafico_linha(self, celula: str,  comprimento: float, altura: float, titulo: str, titulo_eixo_x: str, titulo_eixo_y: str,
+                               referencia_eixo_x: Reference, referencia_eixo_y: Reference,
+                               propriedade_grafico: list):
+
+        grafico = LineChart()
+        grafico.width = comprimento
+        grafico.height = altura
+        grafico.title = titulo
+        grafico.x_axis.title = titulo_eixo_x
+        grafico.y_axis.title = titulo_eixo_y
+        grafico.add_data(referencia_eixo_x)
+        grafico.set_categories(referencia_eixo_y)
+
+        for serie, propriedade in zip(grafico.series, propriedade_grafico):
+            serie.graphicalProperties.line.width = propriedade.grossura
+            serie.graphicalProperties.line.solidFill = propriedade.cor_preenchimento
+
+        self.planilha_ativa.add_chart(grafico, celula)
+
+    def adiciona_imagem(self, celula: str, caminho_imagem: str):
+        imagem = Image(caminho_imagem)
+        self.planilha_ativa.add_image(imagem, celula)
+
+    def salva_arquivo(self, caminho_arquivo: str):
+        self.workbook.save(caminho_arquivo)
--- mainV1.py
+++ mainV1.py
@@ -1,88 +1,88 @@
 from datetime import date
-from openpyxl import Workbook
-from openpyxl.chart import LineChart, Reference
-from openpyxl.drawing.image import Image
+from openpyxl.chart import Reference
 from openpyxl.styles import PatternFill, Font, Alignment
+from classes import LeitorAcoes, GerenciadorPlanilha, PropriedadesSerieGradicos
 
-from classes import LeitorAcoes, GerenciadorPlanilha
+try:
+    # acao = input("QUal o codigo da Ação que você quer processar: ").upper()
 
-# acao = input("QUal o codigo da Ação que você quer processar: ").upper()
+    acao = "BIDI4"
 
-acao = "BIDI4"
+    leitor_acoes = LeitorAcoes(caminho_arquivo='./dados/')
+    leitor_acoes.processa_arquivo(acao)
 
-leitor_acoes = LeitorAcoes(caminho_arquivo='./dados/')
-leitor_acoes.processa_arquivo(acao)
+    gerenciador = GerenciadorPlanilha()
 
+    planilha_dados = gerenciador.adiciona_planilha(titulo_planilha="Dados")
 
-gerenciador = GerenciadorPlanilha()
+    gerenciador.adiciona_linas(["DATA", "COTAÇÂO", "BANDA INFERIOR", "BANDA SUPERIOR"])
 
-planilha_dados = gerenciador.adiciona_planilha(titulo_planilha="Dados")
+    indice = 2
 
-gerenciador.adiciona_linas(["DATA", "COTAÇÂO", "BANDA INFERIOR", "BANDA SUPERIOR"])
+    for linha in leitor_acoes.dados:
+        # DATA
+        # 2018-05-10 21:00:00;0.9969
+        ano_mes_dia = linha[0].split(" ")[0]
+        data = date(
+            year=int(ano_mes_dia.split("-")[0]),
+            month=int(ano_mes_dia.split("-")[1]),
+            day=int(ano_mes_dia.split("-")[2])
+        )
+        # COTAÇÃO
+        cotacao = float(linha[1])
 
-indice = 2
+        # Atualiza celulas
+        formula_bb_inferior = f'AVERANGE(B{indice}:B{indice + 19}) - 2*STDEV(B{indice}:B{indice + 19})'
+        formula_bb_superior = f'AVERANGE(B{indice}:B{indice + 19}) + 2*STDEV(B{indice}:B{indice + 19})'
+        gerenciador.atualiza_celula(celula=f'A{indice}', dado=data)
+        gerenciador.atualiza_celula(celula=f'B{indice}', dado=cotacao)
+        gerenciador.atualiza_celula(celula=f'C{indice}', dado=formula_bb_inferior)
+        gerenciador.atualiza_celula(celula=f'D{indice}', dado=formula_bb_superior)
 
-for linha in leitor_acoes.dados:
-    # DATA
-    # 2018-05-10 21:00:00;0.9969
-    ano_mes_dia = linha[0].split(" ")[0]
-    data = date(
-        year=int(ano_mes_dia.split("-")[0]),
-        month=int(ano_mes_dia.split("-")[1]),
-        day=int(ano_mes_dia.split("-")[2])
+        indice += 1
+
+    gerenciador.adiciona_planilha(titulo_planilha='Grafico')
+    gerenciador.mescla_celula(celula_inicio='A1', celula_fim='T2')
+
+    gerenciador.aplica_estilos(celula='A1',
+                               estilos=[
+                                   ('font', Font(b=True, sz=18, color="FFFFFF")),
+                                   ('fill', PatternFill("solid", fgColor="07838F")),
+                                   ('alignment', Alignment(vertical="center", horizontal="center"))
+                               ])
+
+    gerenciador.atualiza_celula('A1', 'Historico de Cotação')
+
+    referencia_cotacao = Reference(planilha_dados, min_col=2, min_row=2, max_col=4, max_row=indice)
+    referencia_datas = Reference(planilha_dados, min_col=1, min_row=2, max_col=1, max_row=indice)
+
+    gerenciador.adiciona_grafico_linha(
+        celula='A3',
+        comprimento=33.87,
+        altura=14.82,
+        titulo=f'Cotações - {acao}',
+        titulo_eixo_x="Data da Cotação",
+        titulo_eixo_y="Valor da Cotação",
+        referencia_eixo_x=referencia_cotacao,
+        referencia_eixo_y=referencia_datas,
+        propriedade_grafico=[
+            PropriedadesSerieGradicos(grossura=0, cor_preenchimento='0a55ab'),
+            PropriedadesSerieGradicos(grossura=0, cor_preenchimento='a61588'),
+            PropriedadesSerieGradicos(grossura=0, cor_preenchimento='12a154')
+        ]
+
     )
-    # COTAÇÃO
-    cotacao = float(linha[1])
 
-    # Atualiza celulas
-    formula_bb_inferior = f'AVERANGE(B{indice}:B{indice + 19}) - 2*STDEV(B{indice}:B{indice + 19})'
-    formula_bb_superior = f'AVERANGE(B{indice}:B{indice + 19}) + 2*STDEV(B{indice}:B{indice + 19})'
-    gerenciador.atualiza_celula(celula=f'A{indice}', dado=data)
-    gerenciador.atualiza_celula(celula=f'B{indice}', dado=cotacao)
-    gerenciador.atualiza_celula(celula=f'C{indice}', dado=formula_bb_inferior)
-    gerenciador.atualiza_celula(celula=f'D{indice}', dado=formula_bb_superior)
+    gerenciador.mescla_celula(celula_inicio='I32', celula_fim='L35')
+    gerenciador.adiciona_imagem(celula='I32', caminho_imagem='./recursos/b3.png')
 
-    indice += 1
+    gerenciador.salva_arquivo("./saida/Planilha.xlsx")
 
-
-
-
-gerenciador.adiciona_planilha(titulo_planilha='Grafico')
-gerenciador.mescla_celula(celula_inicio='A1', celula_fim='T2')
-gerenciador.estiliza_fonte('A1', Font(b=True, sz=18, color="FFFFFF"))
-
-cabecalho.fill = PatternFill("solid", fgColor="07838F")
-cabecalho.alignment = Alignment(vertical="center", horizontal="center")
-cabecalho.value = "Historico de Cotação"
-
-grafico = LineChart()
-grafico.title = f'Cotações - {acao}'
-grafico.x_axis.title = "Data da Cotação"
-grafico.y_axis.title = "Valor da Cotação"
-
-referencia_cotacao = Reference(planilha_ativa, min_col=2, min_row=2, max_col=4, max_row=indice)
-referencia_datas = Reference(planilha_ativa, min_col=1, min_row=2, max_col=1, max_row=indice)
-
-grafico.add_data(referencia_cotacao)
-grafico.set_categories(referencia_datas)
-
-linha_cotacao = grafico.series[0]
-linha_bb_inferior = grafico.series[1]
-linha_bb_superior = grafico.series[2]
-
-linha_cotacao.graphicalProperties.line.width = 0
-linha_cotacao.graphicalProperties.line.solidFill = "0a55ab"
-
-linha_bb_inferior.graphicalProperties.line.width = 0
-linha_bb_inferior.graphicalProperties.line.solidFill = "a61588"
-
-linha_bb_superior.graphicalProperties.line.width = 0
-linha_bb_superior.graphicalProperties.line.solidFill = "12a154"
-
-planilha_grafico.add_chart(grafico, "A3")
-
-imagem = Image('./recursos/b3.png')
-planilha_grafico.merge_cells("I32:L35")
-planilha_grafico.add_image(imagem, "I32")
-
-workbook.save("./saida/Planilha.xlsx")
+except FileNotFoundError:
+    print('Cota nao encontrada')
+except ValueError:
+    print('Formado de dados incorretos')
+except AttributeError:
+    print('Atributo inexistente')
+except Exception as exececao:
+    print(f'Ocorreu um erro na execução do programa {str(exececao)}')
